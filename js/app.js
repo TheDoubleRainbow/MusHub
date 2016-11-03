d000f74f3c4ddc7f7354f34add1d46755506653b
BuildTools <unconfigured@null.spigotmc.org>
2016-11-03 20:03:51 +0200

    1
    2

--- js/app.js
+++ js/app.js
@@ -1,9 +1,7 @@
-/**
- * Created by andrewkhokhlov on 11/2/16.
- */
+var items = new Array();
 var app = angular.module("MusHub" ,[]);
 app.controller("MainController", ["$scope", "orderByFilter", function($scope, orderBy){
-    $scope.items = [
+    $scope.items = items;/*[
         {
             name: "Song name1",
             rating: 3,
@@ -36,5 +34,40 @@ app.controller("MainController", ["$scope", "orderByFilter", function($scope, or
             album: "basbdb",
             link: "https://cs1-17v4.vk-cdn.net/p5/e313b7ab6ee197.mp3?extra=IVIfFu9vutBJd4xD2mFUako3iNE9gfd0xwcPoT6NPKCjMp_iOCfwypfVpzkUe0CfZ4ViYX5tKhl8Y87G0q_xkY-5nHKB1n28aZO88b8sZDrz6qaz5rvEgwqJgPSN59qUxoJKby6XomunCK0"
         }
-    ];
+    ];*/
 }]);
+ 
+window.onload = function(){
+    VK.init({
+        apiId: 5702688
+        });
+    var id = 0;
+    function authInfo(response) {
+      if (response.session) {
+          id = response.session.mid;
+      }
+    }
+    //VK.Auth.getLoginStatus(authInfo);
+    VK.Auth.login(authInfo, "audio");
+
+    var resp = 0;
+    var onlogin = function(){
+        var count;
+        VK.Api.call('audio.get', {count: '20'}, function(r) {
+            if(r.response) {
+                console.log(r);
+
+            }
+        });
+        /*VK.Api.call('audio.get', {count: '2', owner_id: '224641141'}, function(r) {
+            if(r.response) {
+                //console.log(r.response);
+
+            }
+        });*/
+    }
+    
+        VK.Observer.subscribe("auth.login", onlogin)
+
+    
+}
